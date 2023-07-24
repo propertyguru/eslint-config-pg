eb4d98c9cdcadd103fdc3aa50a35addcdb823d49
Nimit <nimit@ddproperty.com>
2023-07-24 17:21:20 +0700

    feat: Enable JSX transform

---
+++ src/react/example.tsx
@@ -0,0 +1,5 @@
+// Test new JSX transform in React 17.
+// Ref: https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports
+export default function App() {
+  return <h1>Hello World</h1>
+}
